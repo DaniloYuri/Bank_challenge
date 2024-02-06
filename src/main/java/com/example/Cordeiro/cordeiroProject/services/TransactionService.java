963693129ac21823449e90035060f09bdaa446b7
Danilo Cordeiro <144616534+DaniloYuri@users.noreply.github.com>
2024-02-06 17:30:39 -0300

    função authorizeTransaction

--- src/main/java/com/example/Cordeiro/cordeiroProject/services/TransactionService.java
+++ src/main/java/com/example/Cordeiro/cordeiroProject/services/TransactionService.java
@@ -8,6 +8,8 @@ import com.example.Cordeiro.cordeiroProject.repositories.TransactionRepository;
 
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -15,13 +17,14 @@ import org.springframework.web.client.RestTemplate;
 import java.math.BigDecimal;
 import java.util.Map;
 
+import static jakarta.persistence.criteria.Predicate.BooleanOperator.AND;
+
 
 @Service
-public class TransactionService
-{
+public class TransactionService {
 
     @Autowired
-    private TransactionRepository transactionRepository ;
+    private TransactionRepository transactionRepository;
 
     @Autowired
     private UserService userService;
@@ -30,17 +33,32 @@ public class TransactionService
     private RestTemplate restTemplate;
 
 
-    public void createTransaction  ( TransactionDTO obj) throws Exception{
+    public void createTransaction(TransactionDTO obj) throws Exception {
         User sender = this.userService.findUserById(obj.senderId());
         User receiver = this.userService.findUserById(obj.receiverId());
 
-        userService.validateTransaction(sender,obj.value());
+        userService.validateTransaction(sender, obj.value());
+
+        boolean isAuthorized = this.authorizeTransaction(sender, obj.value());
+        if (!isAuthorized) {
+            throw new Exception("Transação não autorizada");
+
+        }
+
+        public boolean authorizeTransaction(User sender, BigDecimal value){
+            ResponseEntity<Map> authozirationResponse = restTemplate.getForEntity("https://run.mocky.io/v3/54dc2cf1-3add-45b5-b5a9-6bf7e7f1f4a6", Map.class);
+
+
+            if (authozirationResponse.getStatusCode() == HttpStatus.OK) {
+                String message = (String) authozirationResponse.getBody().get("message");
+                return "Autorizado".equalsIgnoreCase(message);
+            }
+
+        }
+
     }
-
-    public boolean authorizeTransaction(User sender, BigDecimal value){
-    ResponseEntity<Map>authoziration = restTemplate.getForEntity("https://run.mocky.io/v3/54dc2cf1-3add-45b5-b5a9-6bf7e7f1f4a6", map.class);
-
-
-    }
-
 }
+
+
+
+
